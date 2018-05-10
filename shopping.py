5a0f8d7fc5b709466d445f4fc348f829f4d8313f
Karan Murthy <kmurthy@godaddy.com>
2018-05-09 22:46:46 -0700

    Updated shopping.py

--- shopping.py
+++ shopping.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+shopping_list = ['Milk', 'Eggs']
+
 @app.route('/')
 def index():
-    return '<h1>Shopping List</h1>'
+    return render_template('index.html', items=shopping_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
