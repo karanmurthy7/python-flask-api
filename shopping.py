c568919e86642e7c68931d46c5c03374592be79d
Karan Murthy <kmurthy@godaddy.com>
2018-05-09 23:42:58 -0700

    Updated shopping.py

--- shopping.py
+++ shopping.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 app = Flask(__name__)
 
 
 shopping_list = ['Milk', 'Eggs']
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     global shopping_list
     if request.method == 'POST':
         shopping_list.append(request.form['item'])
     return render_template('index.html', items=shopping_list)
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+@app.route('/remove/<name>')
+def remove_item(name):
+    global shopping_list
+    if name in shopping_list:
+        shopping_list.remove(name)
+    return redirect(url_for('index'))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
